d9d356443811543335a70cfde489aea6d77adb74
Nacho Sancho <sannacho@gmail.com>
2018-09-07 13:20:20 +0200

    listado cervezas ordenadas por nombre

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const cervezas = require('./cervezas.json')
 const uniqueRandomArray = require('unique-random-array')
+const _ = require('lodash')
 
 const prueba = function () {
   return 'Copon'
 }
 
 module.exports = {
-  todas: cervezas,
+  todas: _.sortBy(cervezas, ['nombre']),
   seleccion: 'Ambar Beer',
   alazar: uniqueRandomArray(cervezas)
 }
